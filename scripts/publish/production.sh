edacf431095ced3eae648570d650c944ec20efa9
Mauricio Nobrega <mauricio.nobrega@netshoes.com>
2019-10-18 14:23:18 -0300

    Resolve homepage

---
+++ scripts/publish/production.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd dist &&
+
+git remote add origin https://github.com/mauricionobrega/smartmirror.com.br-www.git &&
+
+git checkout production &&
+
+git add . &&
+
+git commit -f -m \"update version\" &&
+
+git push origin production
